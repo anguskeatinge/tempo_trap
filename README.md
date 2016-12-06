514407516d4846238ab445133de633e3cb15323d
anguskeatinge <angus.keatinge@gmail.com>
2016-12-06 23:38:05 +1100

    added json library, collated beat_tracker scores, collated beat_tracker outputs.

---
+++ README.md
@@ -0,0 +1,15 @@
+Make sure you "addpath" and json.startup to get the json shit working
+
+Todo
+
+Get the json shit working with "test_beats.m" so I can read in nice json objects in "single_score.m" 
+Will be much easier for manipulation in the long run.
+
+simple one number benchmark for each algo
+detailed breakdown of performance
+
+Beat trackers to get (or just results):
+	- ellis' beat_simple
+	- ibt (from Jerry)
+	- BTrack (from Jerry or github)
+
--- test_beats.m
+++ test_beats.m
@@ -1,16 +1,29 @@
 % The reference beats
-ref_dir = 'music/open/_ground_truth/';
+ref_dir = '../music/open/_ground_truth/';
 ref_files = dir( strcat(ref_dir,'*.txt') );
 
-algo_name = '_r2b2';
+pretty_algo_name = 'ground_truth';
+algo_name = strcat('_', pretty_algo_name);
 
 % The beats the algorithm measured
-test_dir = strcat( 'music/open/', algo_name, '/' );
+test_dir = strcat( '../music/open/', algo_name, '/' );
 test_files = dir( strcat( test_dir, '*.txt') );
 
 
-% REading floats
+
+% Reading floats
 formatSpec = '%f';
+
+% set up the filename, there are going to be a bunch of different things to test.
+num = 0;
+while true
+    if exist(strcat('results/', pretty_algo_name, '_', int2str(num), '.txt'), 'file') == 2
+        num = num + 1;
+    else
+        outfile = strcat('results/', pretty_algo_name, '_', int2str(num), '.txt');
+        break
+    end
+end
 
 for file = [ ref_files'; test_files' ]
     % I should make sure that I'm comparing the right files here.
@@ -18,16 +31,18 @@ for file = [ ref_files'; test_files' ]
     test_file = strcat( test_dir, file(2).name );
 
     fileID = fopen(ref_file,'r');
-    annotations = fscanf(fileID,formatSpec);
+    annotations = fscanf(fileID,formatSpec)
     fclose(fileID);
 
     fileID = fopen(test_file,'r');
-    detections = fscanf(fileID,formatSpec);
+    detections = fscanf(fileID,formatSpec)
     fclose(fileID);
 
     [ mainscore, backupscores ] = beatEvaluator(detections',annotations');
 
-    fileID = fopen(strcat('r2b2_test_results.txt'), 'a+');
+    % need to put the json crap in here so I can print it out all nice
+
+    fileID = fopen(outfile, 'a+');
     fprintf(fileID, 'testing: %s\nresults: %f\nbackup scores: %f %f %f\n\n', test_file, mainscore, backupscores(1), backupscores(2), backupscores(3) );
     fclose(fileID);
 
